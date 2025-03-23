8af47ca2f089f617c518f67fdefda2fded083b65
veeravelS <sveeravel16@gmail.com>
2025-03-23 19:15:52 +0530

    auth file create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import CheckEmail from "./pages/CheckEmail";
-import CheckPassword from "./pages/CheckPassword";
 import AppLayout from "./AppLayout";
 import MessagePage from "./components/MessagePage";
 import Home from "./pages/Home";
 import RegisterPage from "./pages/RegisterPage";
-import AuthLayout from "./AuthLayout";
 import ForgetPassword from "./pages/ForgetPassword";
 import ProtectedRoute from "./ProtectedRoute";
 import Login from "./pages/Login";
+import Auth from "./pages/auth/Auth";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AuthLayout />}>
           <Route path="register" element={<RegisterPage />} />
-          <Route path="email" element={<CheckEmail />} />
-          <Route path="password" element={<CheckPassword />} />
           <Route path="login" element={<Login />} />
+          <Route path="auth" element={<Auth />} />
           <Route path="forget-password" element={<ForgetPassword />} />
-        </Route>
         <Route element={<ProtectedRoute />}>
           <Route path="/" element={<AppLayout />}>
             <Route index element={<Home />} />
---
+++ client/src/pages/auth/Auth.jsx
@@ -0,0 +1,18 @@
+
+
+const Auth = () => {
+  return (
+    <div className="bg-red-500">
+        <div className="container">
+            <div className="left">
+
+            </div>
+            <div className="right">
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Auth
